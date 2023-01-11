1363952666bf0578ef175b06455a12029dd3df45
yashj <shine@159>
2023-01-11 11:43:11 +0530

    files renamed

--- main.py
+++ main.py
@@ -1,57 +1,43 @@
-import os
-import pandas as pd
-from nltk.corpus import stopwords
-
-stop = set(stopwords.words('english'))
-from sklearn.feature_extraction.text import TfidfVectorizer
+from flask import Flask, request, jsonify, redirect, render_template
+from functions import cleaning, load_trained_models, lets_predict
 import pickle
-from nltk.tokenize import word_tokenize
-# nltk.download('punkt', download_dir=os.curdir)
-import re
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the user input
+    user_input = request.form['news_article']
+    print(type(user_input))
+    # user_input = data
+    label_encoder, trained_transformer_model, trained_ml_model = load_trained_models()
+    prediction = check_prediction(user_input=user_input, trained_ml_model=trained_ml_model, trained_transformer_model=trained_transformer_model)
+    # Return the result
+    # label_encoder.inverse_transform(prediction)
+    return jsonify({'category': label_encoder.inverse_transform(prediction).tolist()})
 
 
-def cleaning(text):
-    text = text.lower()
-    text = re.sub(' +', ' ', text)  # removing multiple spaces
-    text = re.sub(r'[^\w\s]', '', text)  # removing special characters
-    tokens = word_tokenize(text)
-    tokens = [word for word in tokens if word not in stop]
-    text = ' '.join(tokens)
-    return text
+
+def check_prediction(user_input, trained_ml_model, trained_transformer_model):
+    # user_input = user_input.apply(cleaning)
+    prediction = lets_predict(lr_model=trained_ml_model, trained_vectorizer= trained_transformer_model, fresh_data= user_input)
+    print("after prediction")
+    # return jsonify({'category': str(prediction)})
+    return prediction
 
 
 
 
-tfidf_vectorizer = TfidfVectorizer(encoding='utf-8',
-                                   ngram_range=(1, 2),
-                                   stop_words=None,  # We cleaned them already
-                                   lowercase=False,  # We converted all to lowercase
-                                   max_df=0.95,
-                                   min_df=10,
-                                   norm='l2',
-                                   sublinear_tf=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
-def load_trained_models():
-    label_encoder = pickle.load(open('model\\label_encoder.pkl', 'rb'))
-    trained_model = pickle.load(open('model\\trained_model.pkl', 'rb'))
-    trained_vectorizer = pickle.load(open('model\\trained_vectorizer.pkl', 'rb'))
-    return label_encoder, trained_vectorizer, trained_model
-
-
-def lets_predict(lr_model, trained_vectorizer, fresh_data):
-    fresh_data_df = pd.DataFrame([fresh_data], columns=['Text'])
-    fresh_data_df['Text'] = fresh_data_df['Text'].apply(cleaning)
-    fresh_data_values = fresh_data_df['Text'].values
-    fresh_data_transformed = trained_vectorizer.transform(fresh_data_values)
-
-    return lr_model.predict(fresh_data_transformed.toarray())
-
-# **Trying one more Sample record**
 
 # sample = pd.DataFrame(["Months before the assembly election in Karnataka, Bharatiya Janata Partys (BJP) senior leader and the state former Chief Minister SM Krishna announced his retirement from active politics on Wednesday"],columns=['Text'])
-# sample = "Months before the assembly election in Karnataka, Bharatiya Janata Partys (BJP) senior leader and the state former Chief Minister SM Krishna announced his retirement from active politics on Wednesday"
-# #
-# trained_vectorizer, trained_model = load_trained_models()
-# #
-# print(lets_predict(trained_model, trained_vectorizer, sample))
+#
+# print(check_prediction(sample['Text']))
