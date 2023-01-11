bc2801e7a818a7e3ecdbb563e53895dc0162dde9
yashj <shine@159>
2023-01-11 21:18:44 +0530

    port removed

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def check_prediction(user_input, trained_ml_model, trained_transformer_model):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 
 
